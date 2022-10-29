de9f3c125646310cdcee27243d4a1023296b34d6
marcoalonsorota <marcoalonsorota@hotmail.com>
2022-10-29 09:18:16 -0500

    Initial Commit

---
+++ Perritos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Perritos
+//
+//  Created by Marco Alonso Rodriguez on 29/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
